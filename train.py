9b6f5f4f4ea59f1ed825986466b7369247490fa5
Siddharth Satyam <siddharth4satyam@gmail.com>
2024-06-06 17:31:15 -0700

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ class TextClassificationModel(nn.Module):
     def __init__(self, hidden_size, num_classes):
         super(TextClassificationModel, self).__init__()
         self.global_avg_pool = nn.AdaptiveAvgPool1d(1)  # Global average pooling
-        self.fc = nn.Linear(hidden_size, num_classes)   # Fully connected layer
+        self.fc = nn.Linear(hidden_size, num_classes)  # Fully connected layer
 
     def forward(self, x):
         # x: [batch_size, sequence_length, hidden_size]
@@ -24,6 +24,7 @@ class TextClassificationModel(nn.Module):
         x = x.squeeze(-1)  # [batch_size, hidden_size]
         x = self.fc(x)  # [batch_size, num_classes]
         return x
+
 
 class ImageAudioModel(nn.Module):
     def __init__(self, num_classes):
@@ -39,36 +40,43 @@ class ImageAudioModel(nn.Module):
         self.audio_encoder = WhisperModel.from_pretrained(self.audio_encoder_name).to(device)
 
         self.image_h_dim = 512
-        self.audio_h_dim = 512
+        self.audio_h_dim = 256
 
-        self.project_image = nn.Conv1d(self.image_h_dim, self.image_h_dim,
-                                  kernel_size=3, stride=1).to(device)
+        self.image_seq_len = 50
+        self.audio_seq_len = 1500
+        
+        self.aligned_seq_len = 32
+        self.llm_dim = 256
 
-        self.project_audio = nn.Conv1d(self.audio_h_dim, self.audio_h_dim,
-                                  kernel_size=3, stride=1).to(device)
+        self.project_image = nn.Conv1d(self.image_seq_len, self.aligned_seq_len,
+                                       kernel_size=1, stride=1).to(device)
+
+        self.project_audio = nn.Conv1d(self.audio_seq_len, self.aligned_seq_len,
+                                       kernel_size=1, stride=1).to(device)
 
         self.transform_image_to_hidden = nn.Linear(self.image_h_dim,
-                                              256).to(device)
+                                                   self.llm_dim).to(device)
         self.transform_audio_to_hidden = nn.Linear(self.audio_h_dim,
-                                              256).to(device)
+                                                   self.llm_dim).to(device)
 
         self.image_align_attention = nn.MultiheadAttention(256,
-                                                      4 * 2,
-                                                      dropout=0,
-                                                      add_bias_kv=False,
-                                                      add_zero_attn=False).to(device)
+                                                           4 * 2,
+                                                           dropout=0,
+                                                           add_bias_kv=False,
+                                                           add_zero_attn=False).to(device)
         self.audio_align_attention = nn.MultiheadAttention(256,
-                                                      4 * 2,
-                                                      dropout=0,
-                                                      add_bias_kv=False,
-                                                      add_zero_attn=False).to(device)
+                                                           4 * 2,
+                                                           dropout=0,
+                                                           add_bias_kv=False,
+                                                           add_zero_attn=False).to(device)
 
         self.embed_tokens = nn.Embedding(self.audio_encoder.config.vocab_size, 256).to(device)
         self.num_classes = num_classes
         self.final_classifier = TextClassificationModel(256, num_classes).to(device)
 
     def forward(self, inputs, inputs_common):
-        image_features = self.image_encoder.visual_projection(self.image_encoder.vision_model(inputs['images']).last_hidden_state)
+        image_features = self.image_encoder.visual_projection(
+            self.image_encoder.vision_model(inputs['images']).last_hidden_state)
         audio_features = self.audio_encoder.encoder(inputs['audios']).last_hidden_state
 
         text_embeddings = self.embed_tokens(inputs_common['input_ids'])
@@ -79,11 +87,12 @@ class ImageAudioModel(nn.Module):
         audio_starts = self.embed_tokens(inputs_common['audio_starts'])
         audio_ends = self.embed_tokens(inputs_common['audio_ends'])
 
-        audio_features = self.project_audio(audio_features.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
+        # audio_features = self.project_audio(audio_features.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
+        audio_features = self.project_audio(audio_features)
         audio_features = self.transform_audio_to_hidden(audio_features)
 
         audio_features = self.audio_align_attention(audio_features.transpose(0, 1),
-                                               token_embeddings, token_embeddings)[0].transpose(0, 1).contiguous()
+                                                    token_embeddings, token_embeddings)[0].transpose(0, 1).contiguous()
 
         audio_inputs = torch.cat([torch.cat([audio_starts, audio_features], dim=1), audio_ends], dim=1)
         text_embeddings = torch.cat([text_embeddings, audio_inputs], dim=1)
@@ -91,11 +100,11 @@ class ImageAudioModel(nn.Module):
         image_starts = self.embed_tokens(inputs_common['image_starts'])
         image_ends = self.embed_tokens(inputs_common['image_ends'])
 
-        image_features = self.project_image(image_features.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
-
+        # image_features = self.project_image(image_features.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
+        image_features = self.project_image(image_features)
         image_features = self.transform_image_to_hidden(image_features)
         image_features = self.image_align_attention(image_features.transpose(0, 1),
-                                               token_embeddings, token_embeddings)[0].transpose(0, 1).contiguous()
+                                                    token_embeddings, token_embeddings)[0].transpose(0, 1).contiguous()
 
         image_inputs = torch.cat([torch.cat([image_starts, image_features], dim=1), image_ends], dim=1)
 
@@ -112,10 +121,10 @@ def dummy_test():
     audio = whisper.load_audio("./aud.wav")
     audio = whisper.pad_or_trim(audio)
     mel = whisper.log_mel_spectrogram(audio)
-    all_audio_mels = mel.unsqueeze(0) # (1, 80, 3000)
+    all_audio_mels = mel.unsqueeze(0)  # (1, 80, 3000)
 
     preprocess = _transform(224)
-    
+
     import matplotlib.pyplot as plt
     frame = preprocess(plt.imread("./img.jpg"))
     frame = frame.unsqueeze(0)
@@ -123,18 +132,18 @@ def dummy_test():
     bs = 1
     tokenizer = AutoTokenizer.from_pretrained("togethercomputer/LLaMA-2-7B-32K")
     inputs_common = {
-                        'image_starts': torch.tensor([tokenizer('<image>')['input_ids']] * bs, dtype=torch.int),
-                        'image_ends': torch.tensor([tokenizer('</image>')['input_ids']] * bs, dtype=torch.int),
-                        'audio_starts': torch.tensor([tokenizer('<audio>')['input_ids']] * bs, dtype=torch.int),
-                        'audio_ends': torch.tensor([tokenizer('</audio>')['input_ids']] * bs, dtype=torch.int),
-                        'input_ids' : torch.tensor([tokenizer('<text>')['input_ids']] * bs, dtype=torch.int),
-                        'input_ide': torch.tensor([tokenizer('</text>')['input_ids']] * bs, dtype=torch.int)
-                    }
+        'image_starts': torch.tensor([tokenizer('<image>')['input_ids']] * bs, dtype=torch.int),
+        'image_ends': torch.tensor([tokenizer('</image>')['input_ids']] * bs, dtype=torch.int),
+        'audio_starts': torch.tensor([tokenizer('<audio>')['input_ids']] * bs, dtype=torch.int),
+        'audio_ends': torch.tensor([tokenizer('</audio>')['input_ids']] * bs, dtype=torch.int),
+        'input_ids': torch.tensor([tokenizer('<text>')['input_ids']] * bs, dtype=torch.int),
+        'input_ide': torch.tensor([tokenizer('</text>')['input_ids']] * bs, dtype=torch.int)
+    }
     inputs = {'audios': all_audio_mels, 'images': frame}
-    
+
     inputs = {k: inputs[k].to(device) for k in inputs}
     inputs_common = {k: inputs_common[k].to(device) for k in inputs_common}
-    
+
     model = ImageAudioModel(num_classes=30)
     model = model.to(device)
     output = model(inputs, inputs_common)
@@ -150,7 +159,7 @@ def evaluate(model, dataloader, step):
     with torch.no_grad():
         for i, data in tqdm(enumerate(dataloader),
                             total=len(dataloader),
-                            leave=False, 
+                            leave=False,
                             desc="Evaluating on validation data"):
             data = {k: data[k].to(device) for k in data}
             output = model(data, inputs_common)
@@ -159,76 +168,77 @@ def evaluate(model, dataloader, step):
             total += data["materials"].size(0)
             correct += (output.argmax(1) == data["materials"]).sum().item()
     model.train()
-    
-    val_loss = total_loss/total
-    accuracy = correct/total
+
+    val_loss = total_loss / total
+    accuracy = correct / total
     print(f"Validation Loss: {val_loss}, Accuracy: {accuracy}")
     if log_wandb:
         wandb.log({"val/loss": val_loss, "val/accuracy": accuracy}, step)
 
-    return total_loss/total, correct/total
+    return total_loss / total, correct / total
 
 
 def main(log_name="finetuning"):
     global inputs_common
-    
-    train_loader, val_loader = create_dataloaders(root_dir="/home/GreatestHits/vis-data-256", batch_size=batch_size, val_ratio=eval_ratio)
+
+    train_loader, val_loader = create_dataloaders(root_dir="/home/GreatestHits/vis-data-256", batch_size=batch_size,
+                                                  val_ratio=eval_ratio)
 
     bs = train_loader.batch_size
     all_material_names = train_loader.dataset.dataset.all_material_names
     num_classes = len(all_material_names)
     lr = 3e-4
-    
+
     model = ImageAudioModel(num_classes=num_classes)
-    
+
     if load_filename:
         model.load_state_dict(torch.load(load_filename))
     model = model.to(device)
-    
+
     if freeze_encoders:
         for param in model.image_encoder.parameters():
             param.requires_grad = False
         for param in model.audio_encoder.parameters():
             param.requires_grad = False
-    
+
     if log_wandb:
         wandb.init(project="material-estimation", name=log_name, config={
-                                                                        "epochs": num_epochs,
-                                                                        "batch_size": bs,
-                                                                        "lr": lr,
-                                                                        "freeze_encoders": freeze_encoders,
-                                                                        "image_encoder": model.image_encoder_name,
-                                                                        "audio_encoder": model.audio_encoder_name,
-                                                                        "dataset": "GreatestHits",
-                                                                        })
-    
+            "epochs": num_epochs,
+            "batch_size": bs,
+            "lr": lr,
+            "freeze_encoders": freeze_encoders,
+            "image_encoder": model.image_encoder_name,
+            "audio_encoder": model.audio_encoder_name,
+            "dataset": "GreatestHits",
+        })
+
     tokenizer = AutoTokenizer.from_pretrained("togethercomputer/LLaMA-2-7B-32K")
     inputs_common = {
-                        'image_starts': torch.tensor([tokenizer('<image>')['input_ids']] * bs, dtype=torch.int),
-                        'image_ends': torch.tensor([tokenizer('</image>')['input_ids']] * bs, dtype=torch.int),
-                        'audio_starts': torch.tensor([tokenizer('<audio>')['input_ids']] * bs, dtype=torch.int),
-                        'audio_ends': torch.tensor([tokenizer('</audio>')['input_ids']] * bs, dtype=torch.int),
-                        'input_ids' : torch.tensor([tokenizer('<text>')['input_ids']] * bs, dtype=torch.int),
-                        'input_ide': torch.tensor([tokenizer('</text>')['input_ids']] * bs, dtype=torch.int)
-                    }
-    inputs_common = {k: inputs_common[k].to(device) for k in inputs_common}    
-    
+        'image_starts': torch.tensor([tokenizer('<image>')['input_ids']] * bs, dtype=torch.int),
+        'image_ends': torch.tensor([tokenizer('</image>')['input_ids']] * bs, dtype=torch.int),
+        'audio_starts': torch.tensor([tokenizer('<audio>')['input_ids']] * bs, dtype=torch.int),
+        'audio_ends': torch.tensor([tokenizer('</audio>')['input_ids']] * bs, dtype=torch.int),
+        'input_ids': torch.tensor([tokenizer('<text>')['input_ids']] * bs, dtype=torch.int),
+        'input_ide': torch.tensor([tokenizer('</text>')['input_ids']] * bs, dtype=torch.int)
+    }
+    inputs_common = {k: inputs_common[k].to(device) for k in inputs_common}
+
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     model.train()
-    
+
     loss_values = []
     prev_time = time.time()
-    pbar = tqdm(total=num_epochs*len(train_loader), desc="Training started")
+    pbar = tqdm(total=num_epochs * len(train_loader), desc="Training started")
     step = 0
     curr_epoch = 0
     if load_filename:
         curr_epoch = int(load_filename.split(".")[0].split("_")[1])
-        pbar.update(curr_epoch*len(train_loader))
+        pbar.update(curr_epoch * len(train_loader))
     for epoch in range(curr_epoch, num_epochs):
         for i, data in enumerate(train_loader):
-            data = {k: data[k].to(device) for k in data}    
-            
+            data = {k: data[k].to(device) for k in data}
+
             optimizer.zero_grad()
             output = model(data, inputs_common)
             loss = criterion(output, data["materials"])
@@ -236,12 +246,12 @@ def main(log_name="finetuning"):
             optimizer.step()
 
             loss_values.append(loss.item())
-            
+
             if log_wandb:
                 wandb.log({"train/loss": loss.item()}, step)
             pbar.update(1)
-            pbar.set_description(f"Epoch {epoch+1}, Batch {i+1}, Loss: {loss.item():.4f}")
-            
+            pbar.set_description(f"Epoch {epoch + 1}, Batch {i + 1}, Loss: {loss.item():.4f}")
+
             # Saving progress every 15 mins
             curr_time = time.time()
             if curr_time - prev_time > 900:
@@ -249,14 +259,14 @@ def main(log_name="finetuning"):
                 # print(f"Epoch {epoch+1}, Batch {i+1}, Loss: {loss.item()}")
                 torch.save(model.state_dict(), f"model_{epoch}_{i}.pth")
                 torch.save(loss_values, "loss_history.pth")
-            
+
             if i % 5000 == 0:
                 evaluate(model, val_loader, step)
             step += 1
 
     torch.save(model.state_dict(), "final_model.pth")
     torch.save(loss_values, "loss_history.pth")
-    
+
     return model
 
 
@@ -270,5 +280,5 @@ if __name__ == '__main__':
     load_filename = None
     log_wandb = True
     freeze_encoders = True
-    
+
     main(experiment_name)
